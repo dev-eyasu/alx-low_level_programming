97a5950b2c2cd78fd618ee8d208d6a81fb6751c4
eyasu-dev <you@example.com>
2023-07-13 12:13:57 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * main - A program that prints a line with puts function
+ * Retrun: 0 (Success)
+ * return value of notign
+ */
+
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
